c123d6f3a124f4b504d76bbc6dd387bb94382d3a
madoka305 <1095286992@qq.com>
2025-04-07 21:01:58 +0800

    11

---
+++ bash/startall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$1" == "start" ]; then
+    # 启动所有服务
+    source install/setup.bash
+    ros2 run jk_robot jk_robot_server &
+    echo "机械臂启动"
+    sleep 2
+    sudo chmod 777 /dev/ttyUSB0
+    ros2 run robotiq robotiq_2F85_server &
+    echo "夹爪启动"
+    sleep 2
+    ros2 launch teleop_twist_joy teleop-launch.py &
+    echo "手柄启动"
+elif [ "$1" == "stop" ]; then
+    # 停止所有服务
+    pkill -f "jk_robot_server"
+    pkill -f "robotiq_2F85_server"
+    pkill -f "joy_node"
+    pkill -f "teleop_node"
+    echo "所有服务已停止"
+else
+    echo "Usage: $0 {start|stop}"
+fi
---
+++ bash/startcamera.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+
+if [ "$1" == "start" ]; then
+    serials=($(rs-enumerate-devices | grep "Serial Number" | grep -v "Asic" | awk '{print "_"$NF}'))
+
+    if [ ${#serials[@]} -eq 0 ]; then
+        echo "❌ 没有检测到任何 RealSense 摄像头。"
+        exit 1
+    fi
+
+    echo "✅ 检测到 ${#serials[@]} 个摄像头："
+    for i in "${!serials[@]}"; do
+        echo "  camera$i : ${serials[$i]}"
+    done
+
+    # 启动每个摄像头对应的 ROS 节点
+    for i in "${!serials[@]}"; do
+        echo "🚀 启动 camera$i..."
+        ros2 launch realsense2_camera rs_launch.py \
+            camera_name:=camera$i \
+            serial_no:=${serials[$i]} \
+            enable_depth:=false \
+            rgb_camera.profile:=640x480x30 &
+
+        sleep 1  # 给每个节点一点启动时间
+    done
+
+    echo "🎉 总计 ${#serials[@]} 个摄像头节点已启动！"
+elif [ "$1" == "stop" ]; then
+    # 停止所有服务
+    pkill -f realsense2_camera
+    echo "所有摄像头已停止"
+else
+    echo "Usage: $0 {start|stop}"
+fi
